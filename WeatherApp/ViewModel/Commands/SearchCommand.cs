9b5efb0210d842308bb3a246ec5256f36c38e292
Mike Liao <mike.hm.liao@gmail.com>
2020-01-21 00:10:27 +0800

    bind search command with view

--- WeatherApp/ViewModel/Commands/SearchCommand.cs
+++ WeatherApp/ViewModel/Commands/SearchCommand.cs
@@ -12,15 +12,29 @@ namespace WeatherApp.ViewModel.Commands
 
         public WeatherViewModel WeatherVM { get; set; }
 
-        public event EventHandler CanExecuteChanged;
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
 
         public SearchCommand(WeatherViewModel vm)
         {
             WeatherVM = vm;
         }
 
+        /// <summary>
+        /// evaluate parameter so that search only executes when query is not empty
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
         public bool CanExecute(object parameter)
         {
+            string query = parameter as string;
+
+            if (string.IsNullOrWhiteSpace(query))
+                return false;
+
             return true;
         }
 
